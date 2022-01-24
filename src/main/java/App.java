d3fe0bc3c163921664329da1bc480fa45d883eb1
Xavier <hectormarin2000@gmail.com>
2022-01-24 11:16:20 +0100

    Second Commit+ç

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,72 @@
-public class App {
+import Models.Sala;
 
+import java.util.Scanner;
+
+public class App {
+private static int NUM_SALAS = 3;
+    public static void main(String[] args) {
+        Sala[] sala = new Sala[NUM_SALAS];
+        for (int i = 0; i < NUM_SALAS; i++) {
+            sala[i] = new Sala(5, 9);
+        }
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        char fila = ' ';
+        int columna = 0;
+        int numSala = 0;
+
+        do {
+            option = menu();
+            switch (option) {
+                case 0:
+                    System.out.println("Hasta la Proxima");
+                    break;
+                case 1:
+                    numSala = getNumSala(sala, sc);
+                    sala[numSala].printSala();
+                    System.out.println("Seleccione una Fila: ");
+                    fila = sc.next().charAt(0);
+                    System.out.println("Seleccione un número de butaca:");
+                    columna = sc.nextInt();
+                    if (!sala[numSala].itsFree(fila, columna)) {
+                        System.out.println("Butaca Ocupada.");
+                    } else {
+                        sala[numSala] = sala[numSala].reservaButacas(fila, columna);
+                    }
+                    break;
+            }
+        }while(option != 0);
+    }
+
+    private static int getNumSala(Sala[]sala, Scanner sc) {
+        System.out.println("Salas que están disponibles: ");
+        for (int i = 0; i < sala.length; i++) {
+            System.out.println("Sala"+(i+1));
+        }
+        System.out.println("Seleccione un Número de Sala");
+        int numSala = sc.nextInt();
+        return numSala;
+    }
+
+    private static int menu() {
+        int caso = -1;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Opciones del menu: ");
+        do {
+            System.out.println("0.Salir");
+            System.out.println("1.Ver Sala");
+            System.out.println("2.Reservar");
+            System.out.println("3.Confirmar Reserva");
+            System.out.println("4.Anular Reserva");
+            System.out.println("5.Resumen");
+            try {
+                caso = sc.nextInt();
+            }catch (Exception e){
+                System.out.println("Introduzca una opción de 0-5");
+                sc.next();
+            }
+        }while (caso < 0 || caso > 5);
+        return caso;
+    }
 }
--- src/main/java/Models/Sala.java
+++ src/main/java/Models/Sala.java
@@ -1,5 +1,7 @@
 package Models;
 
+import com.diogonunes.jcolor.Ansi;
+import com.diogonunes.jcolor.Attribute;
 import com.sun.jdi.request.InvalidRequestStateException;
 
 public class Sala {
@@ -8,11 +10,14 @@ public class Sala {
     private final int MAX_COLUMNA = 9;
     private final int MAX_FILA = 5;
     private Butaca[][] matrizButaca;
+    private int recaudacion;
+    private final String filas = "ABCDE";
 
     public Sala(int filas, int columnas) {
         setFilas(filas);
         setColumnas(columnas);
         matrizButaca = new Butaca[filas][columnas];
+        recaudacion = 0;
     }
 
     public int getFilas() {
@@ -47,6 +52,35 @@ public class Sala {
         this.matrizButaca = matrizButaca;
     }
 
+    public int getRecaudacion() {
+        return recaudacion;
+    }
+
+    public void setRecaudacion(int recaudacion) {
+        this.recaudacion = recaudacion;
+    }
+    public int increaseRecaudacion(){
+        setRecaudacion(getRecaudacion() + 6);
+        return getRecaudacion();
+    }
+    public int decreaseRecaudacion(){
+        if (getRecaudacion() >= 6){
+            setRecaudacion(getRecaudacion() - 6);
+            return getRecaudacion();
+        }else return -1;
+    }
+    public boolean itsFree(char fila, int columna){
+        return (matrizButaca[filas.indexOf(fila)][columna - 1].getEstado() == EstadoButacas.LIBRE);
+    }
+    public Sala reservaButacas(char fila,int columna){
+        if (itsFree(fila,columna)){
+            matrizButaca[filas.indexOf(fila)][columna - 1].setEstado(EstadoButacas.RESERVADA);
+        }else{
+            System.out.println("La butaca está ocupada, elija otra.");
+        }
+        return null;
+    }
+
     @Override
     public String toString() {
         String result = "";
@@ -57,6 +91,24 @@ public class Sala {
             result+= "\n";
         }
         return result;
+    }
+
+    public void printSala(){
+        com.diogonunes.jcolor.Attribute gb = Attribute.GREEN_BACK();
+        com.diogonunes.jcolor.Attribute rb = Attribute.RED_BACK();
+        com.diogonunes.jcolor.Attribute bb = Attribute.BLUE_BACK();
+        for (int i = 0; i < MAX_FILA; i++) {
+            System.out.print(filas.charAt(i));
+            for (int j = 0; j < MAX_COLUMNA; j++) {
+                if (matrizButaca[i][j].getEstado() == EstadoButacas.LIBRE){
+                    System.out.print(Ansi.colorize("["+j+"]",gb));
+                }else if (matrizButaca[i][j].getEstado()==EstadoButacas.RESERVADA){
+                    System.out.print(Ansi.colorize("["+j+"]",bb));
+                }else
+                    System.out.print(Ansi.colorize("["+j+"]",rb));
+            }
+            System.out.println("");
+        }
     }
 }
 
